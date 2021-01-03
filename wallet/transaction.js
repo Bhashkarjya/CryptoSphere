cceb7ee4edd6d8410cbf70bc080dca62be4645ea
Bhashkarjya Nath <bhashkarjyanath1999@gmail.com>
2021-01-03 16:18:23 +0530

    validated Transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v1');
+const { verifySignature } = require('../util/index');
 
 class Transaction{
     constructor({ senderWallet, recipient, amount}){
@@ -21,6 +22,28 @@ class Transaction{
             address: senderWallet.publicKey,
             signature: senderWallet.sign(outputMap)
         };
+    }
+
+    static validTransaction( transaction ){
+        
+        const {input,outputMap} = transaction;
+        const {address,amount,signature} = input;
+
+        const outputTotal= Object.values(outputMap).reduce((total, amount) => total+amount);
+        
+        if(amount != outputTotal) {
+            console.error(`Invalid Transaction from ${address}`);
+            return false;
+        }
+
+        if(!verifySignature({ publicKey: address, data: outputMap, signature})){
+            console.error(`Invalid signature from ${address}`);
+            return false;
+        }
+
+        return true;
+
+
     }
 }
 module.exports = Transaction;
